dff641091fd152e6def5dc72f7fbf4ab3947d051
Michael Cheich <michaelcheich@Michaels-iMac-2.local>
2020-10-27 15:00:16 -0400

    first commit

---
+++ my-app/src/test/java/com/mycompany/app/MoneyTest.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
